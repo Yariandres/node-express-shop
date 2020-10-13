db6f02cae9111ec729a055088bfde76aa6003e05
Yari Andres <yari.andres@gmail.com>
2020-10-13 23:08:21 +0200

    create amain layout file  pug

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404');
+  res.status(404).render('404', {pageTitle: '404 Page'});
 });
 
 const port = 5000;
