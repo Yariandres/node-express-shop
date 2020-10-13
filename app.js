3a4ca89e70ba465bc65f4060097804f5773c489d
Yari Andres <yari.andres@gmail.com>
2020-10-13 22:04:43 +0200

    adding pug tamplating engine

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
