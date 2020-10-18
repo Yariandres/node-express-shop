85ad3d8616dffee3630f4bccb055870617947caa
Yari Andres <yari.andres@gmail.com>
2020-10-18 23:14:59 +0200

    adding controller files

--- routes/admin.js
+++ routes/admin.js
@@ -1,24 +1,14 @@
 const path = require('path');
 const express = require('express');
-const rootDir = require('../util/path'); 
+
+const productController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
+router.get('/add-product', productController.getAddProduct);
 
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', { 
-    path: '/admin/add-product', 
-    pageTitle: 'Add Products' 
-  });
-});
+router.post('/add-product', productController.postAddProduct);
 
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
 
 // next(); next() => this allowes the request to continue to the next middleware in line
