bb21b9e702169626eb7222a3e6bee0f78a9da309
Yari Andres <yari.andres@gmail.com>
2020-10-18 23:21:00 +0200

    finishing controllers

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const errorController = require('./controllers/error');
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -14,9 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', {pageTitle: '404 Page'});
-});
+app.use(errorController.get404);
 
 const port = 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
