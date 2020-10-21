1c6f9520b685083216920dea953d1e94cefb8e2d
Yari Andres <yari.andres@gmail.com>
2020-10-21 23:24:17 +0200

    dispaly product data dynamically

--- controllers/admin.js
+++ controllers/admin.js
@@ -16,8 +16,8 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
 
-
-  const product = new Product(req.body.title);
+  const product = new Product(title, imageUrl, price, description);
+  
   product.save();
   res.redirect('/');
 };
