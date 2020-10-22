11de26eb0366a134123aec5336f2e93a7c811262
Yari Andres <yari.andres@gmail.com>
2020-10-22 23:18:40 +0200

    adding product id

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,13 +1,13 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'All Products',
-      path: '/products'
+    Product.fetchAll(products => {
+        res.render('shop/product-list', {
+        prods: products,
+        pageTitle: 'All Products',
+        path: '/products'
+        });
     });
-  });
 };
 
 exports.getIndex = (req, res, next) => {
