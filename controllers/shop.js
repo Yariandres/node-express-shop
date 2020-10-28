1d9a6f00fa8e63960ab750183335e4dba93d5f97
Yari Andres <yari.andres@gmail.com>
2020-10-28 16:54:17 +0100

    adding delete functionality fixed

--- controllers/shop.js
+++ controllers/shop.js
@@ -34,9 +34,26 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
+  Cart.getCart( cart => {
+    Product.fetchAll(products => {
+
+      const cartProducts = [];
+
+      for (product of products) {
+
+        const cartProducData = cart.products.find(prod => prod.id === product.id);
+
+        if(cartProducData) {
+          cartProducts.push({productData: product, qty: cartProducData.qty });
+        };
+      }
+
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts
+      });
+    });
   });
 };
 
