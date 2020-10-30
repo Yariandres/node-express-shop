37b5ce20e32afa20669f0bdd6581115e857bdb3e
Yari Andres <yari.andres@gmail.com>
2020-10-30 20:24:22 +0100

    retriving data from db

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const app = express();
 const path = require('path');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products')
+    .then(result => {
+        console.log(result[0]);
+    })
+    .catch(err => {
+        console.log(err);
+    })
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
--- util/database.js
+++ util/database.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
 
-const pool = mysql.creatPool({
+const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    databse: 'node-complete',
+    database: 'node-complete',
     password: 'ewrewiuy*&^tYT'
 });
 
