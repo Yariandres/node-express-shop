e9b27214fa78acdb38b733ba5d22b7e74b9e6955
Yari Andres <yari.andres@gmail.com>
2020-10-31 14:59:48 +0100

    displaying product details

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const app = express();
 const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const db = require('./util/database');
 
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -12,7 +14,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
@@ -20,5 +22,4 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-const port = 5000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(3000);
--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
 const db = require('../util/database');
+
 const Cart = require('./cart');
 
 module.exports = class Product {
@@ -10,20 +11,20 @@ module.exports = class Product {
     this.price = price;
   }
 
-    save() {
-        return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-        [this.title, this.price, this.imageUrl, this.description]);
-    }
-
-  static deleteById(id) {
-    
+  save() {
+    return db.execute(
+      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+      [this.title, this.price, this.imageUrl, this.description]
+    );
   }
+
+  static deleteById(id) {}
 
   static fetchAll() {
     return db.execute('SELECT * FROM products');
   }
 
-    static findById(id) {
-        return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-    }
+  static findById(id) {
+    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
+  }
 };
