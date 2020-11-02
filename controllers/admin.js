a1d94eef617b9033ecf638a6e6f4d9996f7b402e
Yari Andres <yari.andres@gmail.com>
2020-11-02 22:36:35 +0100

    deleting realted items

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,14 +13,17 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  
-  Product.create({ 
-    title,
-    price,
-    imageUrl,
-    description
-  })
-    .then(() => console.log('Created Product'))
+
+  req.user.createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description
+    })
+    .then(result => {
+      console.log('Created Product')
+      res.redirect('/admin/products')
+    })
     .catch(err => console.log(err))
 };
 
@@ -30,15 +33,26 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId).then(product => {
+
+  req.user
+  .getProducts({ id: prodId })
+  .then(products => {
+
+    const product = products[0];
+
     if (!product) {
+
       return res.redirect('/');
+
     }
+
     res.render('admin/edit-product', {
+
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing: editMode,
       product: product
+
     });
   }).catch(err => console.log(err));
 };
@@ -69,8 +83,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product
-    .findAll()
+  req.user
+  .getProducts()
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -83,6 +97,14 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product
+    .findByPk(prodId)
+    .then(product => {
+      return product.destroy()
+    })
+    .then(result => {
+      console.log('DESTROYED PRODUCT:', result)
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
 };
