8feda777b6c76f1794ed1690fb0c26501c31ba45
Yari Andres <yari.andres@gmail.com>
2020-11-03 13:00:32 +0100

    adding checkout btn

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
-
-
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
+Order.belongsToMany(Product, { through: OrderItem });
 
 const app = express();
 
@@ -50,6 +51,8 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
 
 sequelize
     // .sync({ force: true })
