35c8d3e587ac028bdcb40c1f18d49f7e61f27896
Yari Andres <yari.andres@gmail.com>
2020-11-17 23:45:19 +0100

    adding the add to cart functionality

--- app.js
+++ app.js
@@ -22,7 +22,12 @@ app.use((req, res, next) => {
     User
         .findById('5fb1a15f01dfb4081a38f37c')
         .then(user => {
-            req.user = user;
+            req.user = new User(
+                user.name, 
+                user.email, 
+                user.cart, 
+                user._id
+            );
             next();
         })
         .catch(err => console.log(err))
